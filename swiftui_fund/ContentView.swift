600f372546417d23db5bf4d37cdb0b3c76f91cf1
mohammadaboelnasr <m.aboelnasr@live.com>
2023-12-22 20:38:23 +0300

    base

---
+++ swiftui_fund/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  swiftuifundmentals
+//
+//  Created by Mohammad Aboelnasr on 18/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                Text("Cairo")
+                    .font(.system(size: 50))
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding(.top, 50)
+                VStack{
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                }
+           
+                Spacer()
+                
+            }
+
+            
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui_fund/swiftui_fundApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_fundApp.swift
+//  swiftui_fund
+//
+//  Created by Mohammad Aboelnasr on 22/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_fundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
