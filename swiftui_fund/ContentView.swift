ab2aeac3c9ef8d40e051e33dd7bf63e961285049
mohammadaboelnasr <m.aboelnasr@live.com>
2023-12-23 09:50:15 +0300

    add array of cards

--- swiftui_fund/ContentView.swift
+++ swiftui_fund/ContentView.swift
@@ -8,7 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    let states = ["cloud.sun.fill", "cloud.sun.rain.fill", "cloud.sun.bolt.fill", "cloud", "sun.max.fill", "wind", "snow", "tornado", "cloud.sun.bolt.fill"]
     var body: some View {
+        
         ZStack() {
             
             LinearGradient(gradient: Gradient(colors: [.blue, .white]),
@@ -16,34 +18,13 @@ struct ContentView: View {
                            endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
             
-            VStack() {
-                Text("Cairo")
-                    .font(.system(size: 50))
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .padding(.top, 50)
-                VStack() {
-                    Image(systemName: "cloud.sun.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 180, height: 180)
+//         CardView()
+            HStack {
+                ForEach(states, id: \.self) { state in
+                    CicularView(content: state)
                 }
-                Text("32°")
-                    .font(.system(size: 70))
-                    .foregroundColor(.white)
-                    .padding(.top, 50)
-                VStack(){
-                    Text("Tuesday, December 18")
-                        .font(.system(size: 20))
-                        .foregroundColor(.white)
-                        .padding(.top, 50)
-                }
-           
-                Spacer()
-                
             }
+            
 
             
         }
@@ -51,6 +32,68 @@ struct ContentView: View {
     }
 }
 
+struct CardView: View {
+    var body: some View{
+    let myCardView =  VStack() {
+            Text("Cairo")
+                .font(.system(size: 50))
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .padding(.top, 50)
+            VStack() {
+                Image(systemName: "cloud.sun.fill")
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 180, height: 180)
+            }
+            Text("32°")
+                .font(.system(size: 70))
+                .foregroundColor(.white)
+                .padding(.top, 50)
+            VStack(){
+                Text("Tuesday, December 18")
+                    .font(.system(size: 20))
+                    .foregroundColor(.white)
+                    .padding(.top, 50)
+            }
+       
+            Spacer()
+            
+        }
+        myCardView
+            .padding(.all, 20)
+           
+    }
+}
+
+struct CicularView: View {
+    let content:String
+   @State var isShowing:Bool = false
+    var body: some View {
+        ZStack {
+            Circle()
+                .foregroundColor(.white)
+                .frame(width: 100, height: 100)
+            if isShowing{
+                Image(systemName: content)
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                    
+            }else
+            {
+                Image(systemName: "questionmark")
+            }
+               
+        }
+        .onTapGesture {
+            isShowing.toggle()
+        }
+    }
+}
 #Preview {
     ContentView()
 }
