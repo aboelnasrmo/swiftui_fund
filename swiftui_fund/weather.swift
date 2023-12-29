4a9bf2c44c56b1b1b9ab306582f3d12b4956ae75
mohammadaboelnasr <m.aboelnasr@live.com>
2023-12-29 14:10:29 +0300

    extract the button to another file

--- swiftui_fund/weather.swift
+++ swiftui_fund/weather.swift
@@ -129,18 +129,4 @@ struct MainCityWeatherView: View {
     }
 }
 
-struct WeatherButton: View {
-    var buttonText:String
-    var textColor:Color
-    var backGroundColor:Color
-    var body: some View {
-        
-            Text(buttonText)
-                .frame(width: 280,height: 50)
-                .background(backGroundColor)
-                .foregroundColor(textColor)
-                .font(.system(size: 20,weight: .bold,design: .default))
-                .cornerRadius(20)
-        
-    }
-}
+
