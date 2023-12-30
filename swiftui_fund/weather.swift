b75f015c064bafa2ea3abb82f9c7f6157e49169b
mohammadaboelnasr <m.aboelnasr@live.com>
2023-12-30 21:10:35 +0300

    update the app to IOS 16 new code

--- swiftui_fund/weather.swift
+++ swiftui_fund/weather.swift
@@ -88,8 +88,11 @@ struct WeatherDayView: View {
 struct BackgroundView: View {
      var isNight:Bool
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue, isNight ? .gray : Color("lightBlue")]), startPoint: .topLeading, endPoint: .bottomTrailing)
-            .edgesIgnoringSafeArea(.all)
+//        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue, isNight ? .gray : Color("lightBlue")]), startPoint: .topLeading, endPoint: .bottomTrailing)
+//            .edgesIgnoringSafeArea(.all)
+        ContainerRelativeShape()
+            .fill(isNight ? Color.black.gradient : Color.blue.gradient)
+            .ignoresSafeArea()
     }
 }
 
